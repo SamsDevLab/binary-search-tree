fa1251342f3702e0ab46aea8194f162da1de7795
Sam Hadfield <sthtech@protonmail.com>
2025-09-12 12:00:42 -0500

    Create file to test bst.js

---
+++ main.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import { Tree } from "./bst.js";
+
+const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const newTree = new Tree(arr);
+
+// console.log(newTree);
