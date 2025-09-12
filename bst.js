b5df9facba12f326acb3b067747a7746005ec046
Sam Hadfield <sthtech@protonmail.com>
2025-09-12 12:07:17 -0500

    Add 'Node'/'Tree' classes, recursion/buildTree fns
    Add Node and Tree classes. Create 'buildTree' which sorts array and calls
    createNodeRecursively until tree is complete. Returns binary tree to
    this.root

---
+++ bst.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export class Tree {
+  constructor(array) {
+    this.root = buildTree(array);
+  }
+}
+
+const createNodeRecursively = (arr, start, end) => {
+  if (start > end) return null;
+
+  let mid = Math.floor((start + end) / 2);
+
+  const root = new Node(arr[mid]);
+
+  root.left = createNodeRecursively(arr, start, mid - 1);
+  root.right = createNodeRecursively(arr, mid + 1, end);
+
+  return root;
+};
+
+const buildTree = (array) => {
+  const sortedArr = array.sort((a, b) => a - b);
+  const uniqueArr = [...new Set(sortedArr)];
+
+  const start = 0;
+  const end = uniqueArr.length - 1;
+
+  const root = createNodeRecursively(uniqueArr, start, end);
+
+  // Print Binary Tree
+  const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
+  prettyPrint(root);
+
+  return root;
+};
