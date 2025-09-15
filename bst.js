30433d3f67b3b306d91480e65de978bb86472e91
Sam Hadfield <sthtech@protonmail.com>
2025-09-15 08:46:02 -0500

    Move buildTree/createNodeRecursively into Tree class

--- bst.js
+++ bst.js
@@ -10,48 +10,42 @@ class Node {
 
 export class Tree {
   constructor(array) {
-    this.root = buildTree(array);
+    this.root = this.buildTree(array);
   }
 
-  insert(root, value) {
-    if (root === null) return new Node(value);
+  insert(value) {
+    const root = this.root;
 
-    if (root.data === value) return root;
+    console.log(root);
+    console.log(value);
+  }
 
-    if (value < root.data) {
-      root.left = this.insert(root.left, value);
-    } else if (value > root.data) {
-      root.right = this.insert(root.right, value);
-    }
+  createNodeRecursively(arr, start, end) {
+    if (start > end) return null;
+
+    let mid = Math.floor((start + end) / 2);
+
+    const root = new Node(arr[mid]);
+
+    root.left = this.createNodeRecursively(arr, start, mid - 1);
+    root.right = this.createNodeRecursively(arr, mid + 1, end);
+
+    return root;
+  }
+
+  buildTree(array) {
+    console.log(array);
+    const sortedArr = array.sort((a, b) => a - b);
+    const uniqueArr = [...new Set(sortedArr)];
+
+    const start = 0;
+    const end = uniqueArr.length - 1;
+
+    const root = this.createNodeRecursively(uniqueArr, start, end);
 
     return root;
   }
 }
-
-const createNodeRecursively = (arr, start, end) => {
-  if (start > end) return null;
-
-  let mid = Math.floor((start + end) / 2);
-
-  const root = new Node(arr[mid]);
-
-  root.left = createNodeRecursively(arr, start, mid - 1);
-  root.right = createNodeRecursively(arr, mid + 1, end);
-
-  return root;
-};
-
-const buildTree = (array) => {
-  const sortedArr = array.sort((a, b) => a - b);
-  const uniqueArr = [...new Set(sortedArr)];
-
-  const start = 0;
-  const end = uniqueArr.length - 1;
-
-  const root = createNodeRecursively(uniqueArr, start, end);
-
-  return root;
-};
 
 // Print Binary Tree
 export const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -66,3 +60,17 @@ export const prettyPrint = (node, prefix = "", isLeft = true) => {
     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
   }
 };
+
+// insert(root, value) {
+//   if (root === null) return new Node(value);
+
+//   if (root.data === value) return root;
+
+//   if (value < root.data) {
+//     root.left = this.insert(root.left, value);
+//   } else if (value > root.data) {
+//     root.right = this.insert(root.right, value);
+//   }
+
+//   return root;
+// }
