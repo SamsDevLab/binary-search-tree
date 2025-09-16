cca748ee16de2a093334709898172f51b2d0dc88
Sam Hadfield <sthtech@protonmail.com>
2025-09-16 15:35:22 -0500

    Add 'delete' and 'find' calls for testing purposes

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
 
 import { Tree } from "./bst.js";
+import { prettyPrint } from "./bst.js";
 
 const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 const newTree = new Tree(arr);
 
-// console.log(newTree);
+// 'Insert' Method:
+// newTree.insert(2);
+// newTree.insert(6);
+
+// 'Delete' Method:
+// newTree.delete(1);
+
+// 'Find' Method
+// console.log(newTree.find(5));
+console.log(newTree.find(67));
+
+prettyPrint(newTree.root);
